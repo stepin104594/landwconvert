7120463ccf4d6f732e7a7cfb29cf871a89484ec3
Kemba Krishna Lalasa <69115484+stepin104594@users.noreply.github.com>
2020-09-19 10:01:09 +0530

    upload

---
+++ inc/pro1fun.h
@@ -0,0 +1,7 @@
+#ifndef FUNCT_H_INCLUDED
+#define FUNCT_H_INCLUDED
+void convertsit(int SEL);
+
+
+
+#endif // FUNCT_H_INCLUDED
---
+++ pro1main.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include "pro1fun.h"
+void main()
+{
+int NUM,N,WEI,W,SEL;
+float DISTANCE,METERS,WEIGHT,GRAMS;
+printf("SELECT CONVERSION CHOICE");
+printf("\n 1. LENGTH CONVERSION\n");
+printf("\n 2. WEIGHT CONVERSION\n");
+scanf("%d",&SEL);
+convertsit(SEL);
+/*
+void millitocenti(float METERS,float DISTANCE){
+METERS=DISTANCE/10;
+    printf("\n %f MILLIMETERS IS %f CENTIMETERS\n",DISTANCE,METERS);
+}
+void millitometer(float METERS,float DISTANCE){
+METERS=DISTANCE/1000;
+    printf("\n %f MILLIMETERS IS %f METERS\n",DISTANCE,METERS);
+}
+if(SEL == 1){
+        printf("SELECTED LENGTH CONVERSION");
+printf("SELECT MEASURING METRICS");
+printf("\n 1. MILLIMETERS\n");
+printf("\n 2. CENTIMETERS\n");
+printf("ENTER CHOICE HERE :");
+scanf("%d",&NUM);
+if(NUM == 1) {
+    printf("SELECT CONVERSION");
+    printf("\n 1. MILLIMETERS TO CENTIMETERS\n");
+    printf("\n 2. MILLIMETERS TO METERS\n");
+    printf("ENTER CHOICE HERE :");
+    scanf("%d",&N);
+    switch(N)
+    {
+    case 1 : printf("\n SELETED MILLIMETERS TO CENTIMETERS\n");
+    break;
+    case 2 : printf("\n SELETED MILLIMETERS TO METERS\n");
+    break;
+    default : printf("YOU HAVE ENTERED WRONG CHOICE !!!");
+    }
+    printf("ENTER WEIGHT HERE :");
+    scanf("%f",&DISTANCE);
+    switch(N)
+    {
+    case 1 : millitocenti(METERS,DISTANCE);
+    break;
+    case 2 : millitometer(METERS,DISTANCE);
+    break;
+    }
+    }
+else {
+    printf("YOU HAVE ENTERED WRONG CHOICE !!!");
+}
+}
+*/
+}
